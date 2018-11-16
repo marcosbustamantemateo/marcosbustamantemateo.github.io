fe9abd74349c16c584af7c85124b882ebe77c691
marcosbustamantemateo <marcosbustamantemateo@gmail.com>
2018-11-17 00:44:09 +0100

    pwa test

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,5 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch(function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
+        navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {}).catch(function(err) { });
     });
 }
