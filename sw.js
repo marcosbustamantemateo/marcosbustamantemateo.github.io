ff5b786b6edc2ed571bfaacde64e2e0391b33aff
marcosbustamantemateo <marcosbustamantemateo@gmail.com>
2018-11-17 01:06:54 +0100

    pwa test

---
+++ sw.js
@@ -0,0 +1,12 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html'
+     ]);
+   })
+ );
+});
