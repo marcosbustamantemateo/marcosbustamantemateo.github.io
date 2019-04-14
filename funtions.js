071d692972c8afd5298e57aa3ab88101edd0f8bd
Marcos B <marcosbustamantemateo@gmail.com>
2019-04-14 14:23:43 +0200

    no message

--- funtions.js
+++ funtions.js
@@ -69,29 +69,22 @@ $(document).ready(function() {
 
 });
 
-$("#logo_home").on("click", function () {
+$("#logoHome").on("click", function () {
 
     if(deferredPrompt !== undefined) {
-        // The user has had a postive interaction with our app and Chrome
-        // has tried to prompt previously, so let's show the prompt.
+
         deferredPrompt.prompt();
-    
-        // Follow what the user has done with the prompt.
         deferredPrompt.userChoice.then(function(choiceResult) {
-    
-          console.log(choiceResult.outcome);
-    
+        
           if(choiceResult.outcome == 'dismissed') {
             console.log('User cancelled home screen install');
           }
           else {
             console.log('User added to home screen');
-          }
-    
-          // We no longer need the prompt.  Clear it up.
+          } 
           deferredPrompt = null;
         });
-      }
+    }
 });
 
 var showText = function(target, message, index, interval) {
