d030b79c5a9aff291981cc65377a79a2a99766c7
Marcos B <marcosbustamantemateo@gmail.com>
2019-04-14 13:42:13 +0200

    no message

--- sw.js
+++ sw.js
@@ -1,10 +1,49 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('mbm').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html'
-     ]);
-   })
- );
+var CACHE_NAME = 'mbm';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/funtions.js',
+  '/logo.png',
+  '/styles.css'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('push', function(event) {
+  var title = 'Yay a message.';
+  var body = 'We have received a push message.';
+  var icon = '/images/smiley.svg';
+  var tag = 'simple-push-example-tag';
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag
+    })
+  );
 });
