cd5b02ee792d3f2b547ba8dbf42f53cb96a7416f
marcosbustamantemateo <marcosbustamantemateo@gmail.com>
2019-07-27 19:55:00 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -23,6 +23,22 @@ self.addEventListener('message', (event) => {
   }
 });
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 workbox.core.clientsClaim();
 
 /**
