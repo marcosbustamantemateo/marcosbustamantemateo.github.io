5cc82aa3492def514a94caf1602302b88147b143
Marcos B <marcosbustamantemateo@gmail.com>
2019-07-27 19:40:52 +0200

    Updates

---
+++ precache-manifest.845220c573ba797eb3f95f0445eb4661.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233e5760e259a87e1317d80cc5963fc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b84b25b3c77c73a0c593",
+    "url": "/static/css/main.bae7f548.chunk.css"
+  },
+  {
+    "revision": "3a8efd6c0a9098b11be5",
+    "url": "/static/js/2.1deb2164.chunk.js"
+  },
+  {
+    "revision": "b84b25b3c77c73a0c593",
+    "url": "/static/js/main.c38394ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1690390a9e1479594a25b6e5d99c5a7d",
+    "url": "/static/media/angular.1690390a.svg"
+  },
+  {
+    "revision": "6704577550de27e7727c8c4d8085c39e",
+    "url": "/static/media/asc.67045775.jpg"
+  },
+  {
+    "revision": "a2ed8e0b0c49aa9a0ef7e7321246e0dc",
+    "url": "/static/media/backgroundHome.a2ed8e0b.jpg"
+  },
+  {
+    "revision": "6d9275ff0a63cbbdc0a2507217d4b1ee",
+    "url": "/static/media/dotnet.6d9275ff.png"
+  },
+  {
+    "revision": "294e33f02a7be23b64bca3778a22a934",
+    "url": "/static/media/fjr.294e33f0.jpg"
+  },
+  {
+    "revision": "da5dd6a1ab216056f9917db6b0ec59d8",
+    "url": "/static/media/gmail.da5dd6a1.png"
+  },
+  {
+    "revision": "638af725813347ca8c7a6b855f12f1f3",
+    "url": "/static/media/graphql.638af725.svg"
+  },
+  {
+    "revision": "f08c524d147f46398cb54013ee7013ee",
+    "url": "/static/media/linkedin.f08c524d.png"
+  },
+  {
+    "revision": "ee73ddf3afed91c82cd940a2370d9db8",
+    "url": "/static/media/mco.ee73ddf3.jpg"
+  },
+  {
+    "revision": "64532411c080e3641ee5e8828adf8332",
+    "url": "/static/media/nodejs.64532411.png"
+  },
+  {
+    "revision": "c7d1db7f52c0762ad6f70074cd2dc30a",
+    "url": "/static/media/photo.c7d1db7f.png"
+  },
+  {
+    "revision": "1391c0c4bf4b16197cfa0a8fd3b4f0cc",
+    "url": "/static/media/react.1391c0c4.svg"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/static/media/redux.59c46753.png"
+  },
+  {
+    "revision": "813a874883b6fc4bb7867eee69f0c4a1",
+    "url": "/static/media/spring.813a8748.png"
+  },
+  {
+    "revision": "c4a66f6a3d91ebb573506ff8d726565c",
+    "url": "/static/media/svn.c4a66f6a.png"
+  },
+  {
+    "revision": "edde926ec82ba97c1dc878dc9fa32cff",
+    "url": "/static/media/whatsapp.edde926e.png"
+  },
+  {
+    "revision": "a836fa596e0dc2cc0200d145dbdf430d",
+    "url": "/static/media/xamarin.a836fa59.png"
+  }
+]);
